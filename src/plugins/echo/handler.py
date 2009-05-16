aa10f627af317480b55cc2d4e6639b49e0422df3
Piotr Husiatyński <phusiatynski@gmail.com>
2009-05-16 17:44:09 +0200

    cleaning echo plugin

--- src/plugins/echo/handler.py
+++ src/plugins/echo/handler.py
@@ -6,7 +6,7 @@ from plugger import plugg_manager
 
 
 def echo(user, channel, message, protocol):
-    print "echo", protocol
-    protocol.say(channel, "echo: %s" % message)
+    nick = user.split("!", 1)[0]
+    protocol.say(channel, "%s: %s" % (nick, message))
 
 plugg_manager.register("privmsg", echo)
