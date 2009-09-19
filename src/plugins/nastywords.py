24a56e1f97ad53a8f39563dc81a17b5fad388aca
Piotr Husiatyński <phusiatynski@gmail.com>
2009-09-19 13:26:46 +0200

    nasty words plugin update

--- src/plugins/nastywords.py
+++ src/plugins/nastywords.py
@@ -9,15 +9,23 @@ from zope.interface import implements
 from core.plugins.manager import plugin_manager
 from core.plugins.interface import IStorable, IActionHandler
 
+from twisted.python import log
+import sys
+log.startLogging(sys.stdout)
 
-CMD_BAN = ':ban'
+
+CMD_BAN = ':ban '
+CMD_LIST = ':banlist '
+CMD_UNBAN = ':unban '
+
 
 class NastyRegexpFilter(object):
 
     implements(IStorable, IActionHandler)
 
-    def __init__(self, name):
+    def __init__(self, name, admins):
         self.name = '%s.%s' % (type(self).__name__, name)
+        self.admins = admins
         self._nasty_rx = []
 
     def dump(self):
@@ -30,25 +38,42 @@ class NastyRegexpFilter(object):
         return action in ['privmsg', ]
 
     def handle_action(self, protocol, action, user, message):
-        if message.startswith(CMD_BAN):
-            message = message.split()
-            rx = re.compile(message[1], re.IGNORECASE|re.UNICODE)
-            reason = ' '.join(message[2:]) or None
-            self._nasty_rx.append((rx, reason))
-            response = 'nasty word: %s  reason: %s' % (message[1], reason)
-            protocol.say(protocol.channel, response)
+        if message.startswith(CMD_BAN) and user in self.admins:
+            # add new filter
+            message = message[len(CMD_BAN):].split()
+            rx = re.compile(message[0], re.IGNORECASE|re.UNICODE)
+            reason = ' '.join(message[1:]) or None
+            self._nasty_rx.append((rx, message[0], reason))
+        elif message.startswith(CMD_LIST) and user in self.admins:
+            # find matching filters and display them
+            rx_filter = message[len(CMD_LIST):]
+            for rx, rx_str, reason in self._nasty_rx:
+                if rx_str.startswith(rx_filter):
+                    response = 'rx: %s   reason: %s' % (rx_str, reason)
+                    protocol.say(protocol.channel, response)
+        elif message.startswith(CMD_UNBAN) and user in self.admins:
+            # find matching filter and remove them
+            rx_filter = message[len(CMD_UNBAN):]
+            for i, (rx, rx_str, reason) in enumerate(self._nasty_rx):
+                if rx_str == rx_filter:
+                    self._nasty_rx.pop(i)
+                    protocol.say(protocol.channel, 'unban: %s' % rx_str)
         else:
+            # check if message contains banned word and kick user if does
             rx, reason = self.find_nasty_word(message)
             if rx:
                 nick = user.split('!', 1)[0]
                 protocol.kick(protocol.channel, nick, reason)
 
     def find_nasty_word(self, message):
-        for rx, reason in self._nasty_rx:
+        for rx, rx_str, reason in self._nasty_rx:
             if rx.search(message):
                 return (rx, reason)
         return (None, None)
 
 
-nasty_filter = NastyRegexpFilter('myfilter')
+admins = set([
+        'JohnDoe!i=john.doe@myname.com',
+    ])
+nasty_filter = NastyRegexpFilter('myfilter', admins)
 plugin_manager.register(nasty_filter)
